6e346ff976461b9293ecc9925f79ed693ec70f69
Mindaugas Barysas <mindaugas.barysas@lendico.de>
2015-11-16 20:40:22 +0200

    - added fast code coverage stats - removed unnecessary coverage annotations

--- test-coverage.sh
+++ test-coverage.sh
@@ -14,8 +14,27 @@ IGNORE_COVERAGE="tests/"
 
 bootstrap_load_module "core/testing/coverage"
 
+command=$1
+test_script=$2
+if [[ $test_script == "" ]]
+then
+    test_script='tests.sh'
+fi
+
 case $1 in
-    'verbose') testing::coverage_verbose "tests.sh" "$IGNORE_COVERAGE";;
-    'summary') testing::coverage_summary "tests.sh" "$IGNORE_COVERAGE";;
-    *) echo "Usage: ./test-coverage.sh verbose|summary";;
+    'verbose') testing::coverage_verbose $test_script "$IGNORE_COVERAGE";;
+    'summary') testing::coverage_summary_exact $test_script "$IGNORE_COVERAGE";;
+    'summary-fast') testing::coverage_summary_fast $test_script "$IGNORE_COVERAGE";;
+    *) cat <<limitmark
+    Usage: ./test-coverage.sh verbose|summary|summary-fast [test-script]
+
+    verbose: print files executed by tests, color-coding the covered lines.
+    summary: print exact summary of coverage.
+    summary-fast: way faster summary, but possibly not as exact.
+
+    test-script: bashwithnails test script. Defaults to "tests.sh"
+
+    Getting >100% coverage? Check your annotations.
+limitmark
+    ;;
 esac
